1e24b2248b9443cad1cfd2459365e48d1087fb9e
K.Charan Teja Reddy <129493881+Charan2437@users.noreply.github.com>
2024-07-29 15:12:02 +0530

    Update config.py

--- server/config.py
+++ server/config.py
@@ -5,8 +5,8 @@ class Config:
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'kcharantejareddy2425@gmail.com'
-    MAIL_PASSWORD = 'ouxdporzshsybgmn'
-    MAIL_DEFAULT_SENDER = 'kcharantejareddy2425@gmail.com'
+    MAIL_USERNAME = 'kch...*****'
+    MAIL_PASSWORD = '************'
+    MAIL_DEFAULT_SENDER = 'kch...*****'
 
 # ouxd porz shsy bgmn
